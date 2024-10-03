d82e9fbf9842ff3779167359e8405e8c14a209f5
teofilorodjay <teofilorodjay@gmail.com>
2024-10-03 09:32:06 +0800

    Add files via upload

---
+++ gradeAssignment.js
@@ -0,0 +1,10 @@
+let score = prompt("Enter your score:");
+if (score >= 90) {
+    console.log("Your grade is: Excellent");
+} else if (score >= 80) {
+    console.log("Your grade is: Good");
+} else if (score >= 70) {
+    console.log("Your grade is: Fair");
+} else {
+    console.log("Your grade is: Needs Improvement"); 
+}
